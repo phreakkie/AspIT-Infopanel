7c8047779d6ef3f6c8df903c5615140aa4da1506
Nicolai Hansen <nicolainille@hotmail.com>
2022-02-07 11:55:29 +0100

    Added JS/API functionalty to weather

--- public/script.js
+++ public/script.js
@@ -16,22 +16,48 @@ fetch("https://api.openweathermap.org/data/2.5/onecall?lat=57.048&lon=9.9187&exc
 	.catch(err => console.error(err))
 
 function weather(data){
-  console.log(data)
-  console.log(Math.round(data.current.temp));
-  console.log(data.daily[1].dt)
-  console.log(data.daily[1].weather[0].description)
-
-
+  setInterval(printCurrent(data.current), 3600000)
+  let firstDate = document.querySelector("#firstDate")
+  let firstIcon = document.querySelector("#firstIcon")
+  let firstTemp = document.querySelector("#firstTemp")
+  let secondDate = document.querySelector("#secondDate")
+  let secondIcon = document.querySelector("#secondIcon")
+  let secondTemp = document.querySelector("#secondTemp")
+  let thirdDate = document.querySelector("#thirdDate")
+  let thirdIcon = document.querySelector("#thirdIcon")
+  let thirdTemp = document.querySelector("#thirdTemp")
+  
   data.daily.forEach((day, i) => {
-    let unix = day.dt
-    const d = new Date(unix * 1000)
-    const n = d.toLocaleDateString("da-DK", {weekday:"short"})
-    console.log('d:', n)
-
     let temp = Math.round(day.temp.day)
     console.log('temp:', temp)
-
     let icon = day.weather[0].icon
-    console.log(`http://openweathermap.org/img/w/${icon}.png`)
+    let iconLink = `http://openweathermap.org/img/wn/${icon}@4x.png`
+    let unix = day.dt
+    const d = new Date(unix * 1000)
+    const weekday = d.toLocaleDateString("da-DK" , {weekday:"short"})
+    if(i==1){
+      firstDate.innerHTML = weekday
+      firstTemp.innerHTML = temp
+      firstIcon.src = iconLink
+    } else if(i == 2){
+      secondDate.innerHTML = weekday
+      secondTemp.innerHTML = temp
+      secondIcon.src = iconLink
+    }else if(i==3){
+      thirdDate.innerHTML = weekday
+      thirdTemp.innerHTML = temp
+      thirdIcon.src = iconLink
+    }
   });
+}
+
+function printCurrent(data){
+  console.log('data:', data)
+  let dailyIcon = document.querySelector("#dailyIcon")
+  let dailyTemp = document.querySelector("#dailyTemp")
+  let icon = data.weather[0].icon
+  let temp = Math.round(data.temp)
+  iconLink = `http://openweathermap.org/img/wn/${icon}@4x.png`
+  dailyIcon.src = iconLink
+  dailyTemp.innerHTML = temp
 }
